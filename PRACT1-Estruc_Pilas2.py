29ad06b0aafaf0cf5880d0de694b6d0a8e58ea9a
Jose cicero <Le23081232@merida.tecnm.mx>
2024-10-10 17:03:16 -0600

    PRACT1-Estruc_Pilas2.py

--- PRACT1-Estruc_Pilas2.py
+++ PRACT1-Estruc_Pilas2.py
@@ -1,8 +1,14 @@
+import matplotlib.pyplot as plt
+
 class Pila:
     def __init__(self, capacidad_maxima=8):
         self.capacidad_maxima = capacidad_maxima
         self.pila = []
         self.tope = 0
+        self.historial_topes = []
+
+    def actualizar_grafico(self):
+        self.historial_topes.append(self.tope)
 
     def insertar(self, elemento):
         if self.tope < self.capacidad_maxima:
@@ -11,6 +17,7 @@ class Pila:
             print(f"Insertado {elemento}. Pila: {self.pila}")
         else:
             print(f"Error: Desbordamiento. No se puede insertar {elemento}, la pila está llena.")
+        self.actualizar_grafico()
 
     def eliminar(self):
         if self.tope > 0:
@@ -19,6 +26,7 @@ class Pila:
             print(f"Eliminado {elemento_eliminado}. Pila: {self.pila}")
         else:
             print(f"Error: Subdesbordamiento. No hay elementos para eliminar, la pila está vacía.")
+        self.actualizar_grafico()
 
 mi_pila = Pila()
 
@@ -34,3 +42,10 @@ mi_pila.insertar('R')
 
 print(f"Elementos finales en la pila: {mi_pila.pila}")
 print(f"Total de elementos en la pila: {mi_pila.tope}")
+
+plt.plot(mi_pila.historial_topes, marker='o')
+plt.title('Evolución de la Pila')
+plt.xlabel('Operaciones')
+plt.ylabel('Cantidad de elementos en la Pila')
+plt.grid(True)
+plt.show()
